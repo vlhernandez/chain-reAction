764d371c61e494e0f42d3077abb9fba8383352f7
Veronica Hernandez <vlhernandez@gmail.com>
2017-09-21 17:38:40 -0500

    por fin!

--- js/scripts.js
+++ js/scripts.js
@@ -7,51 +7,12 @@ const dateNavBtns = dateNav.querySelectorAll('button')
 const addAction = document.querySelector(".add-action");
 const actionsList= document.querySelector('.actions');
 
-const dailyEntries = JSON.parse(localStorage.getItem('dailyEntries')) || {};
 const allActions = JSON.parse(localStorage.getItem('allActions')) || [];
 
 
-//------------- STORE DATE and ACTION -------------
-function storeAction(e) {
-  e.preventDefault();
-  if( !(today in dailyEntries) ) {
-    // make an entry for the date if it hasn't been entered yet
-    dailyEntries[today] = [];
-  }
-
-  if( allActions.length < 1 ){
-    console.log('no actions logged yet')
-  }
-
-  const todayActions = dailyEntries[today];
-  const name = ( this.querySelector('[name=actionName]')).value;
-  const color = ( this.querySelector('[name=actionColor]')).value;
-
-  const action = {
-    name,
-    color,
-  }
-
-  const actionStatus = {
-    name,
-    done: false
-  }
-
-  allActions.push(action);
-  populateActions( allActions, actionsList);
-
-  todayActions.push(actionStatus);
-
-  localStorage.setItem('dailyEntries', JSON.stringify(dailyEntries));
-  localStorage.setItem('allActions', JSON.stringify(allActions));
-
-  this.reset();
-}
 
 
-
-
-//------------- DATE NAVIGATION AND DISPLAY -------------
+//------------- DISPLAY DATE -------------
 function displayDate(date = now) {
   dateNav.querySelector('.current-date').innerHTML = `
       ${date.format('dddd')}<br>
@@ -60,53 +21,72 @@ function displayDate(date = now) {
 }
 
 
+
+//------------- STORE ACTION -------------
+function storeAction(e) {
+  e.preventDefault();
+
+  const name = ( this.querySelector('[name=actionName]')).value;
+  const color = ( this.querySelector('[name=actionColor]')).value;
+
+  const action = {
+    name,
+    color,
+    done: {},
+  }
+  action.done[today] = false;
+
+  allActions.push(action);
+  populateActions( allActions, actionsList);
+
+  localStorage.setItem('allActions', JSON.stringify(allActions));
+
+  this.reset();
+}
+
+
+
+
+//------------- DATE NAVIGATION -------------
 function handleDateNav(e) {
   if (e.target.dataset.datenav == 'goBack') {
     const back = now.subtract(1, 'day')
     displayDate(back);
-    populateActions(dailyEntries, actionsList, back.format("YYYY-M-D"));
+    populateActions(allActions, actionsList, back.format("YYYY-M-D"));
+  }
 
-  } else if (e.target.dataset.datenav == 'goFoward') {
+  else if (e.target.dataset.datenav == 'goForward') {
     if (moment().isSame( now, 'd' )) return
     const forward = now.add(1, 'day');
     displayDate(forward);
-    populateActions(dailyEntries, actionsList, forward.format("YYYY-M-D"));
+    populateActions(allActions, actionsList, forward.format("YYYY-M-D"));
 
-  } else if (e.target.dataset.datenav == "goToToday") {
+  }
+
+  else if (e.target.dataset.datenav == "goToToday") {
     if (moment().isSame( now, 'd' )) return
     now = moment();
     displayDate(now);
-    populateActions(dailyEntries, actionsList, now.format("YYYY-M-D"));
+    populateActions(allActions, actionsList, now.format("YYYY-M-D"));
   }
 }
 
 
-function populateActions( dailyEntries = {}, list, date = today) {
-  const items = dailyEntries[date];
 
-  if ( Object.keys(dailyEntries).length < 1 && Object.keys(allActions).length < 1 ) {
+
+//------------- DISPLAY ACTIONS -------------
+function populateActions( allActions = [], list, date = today ) {
+  if ( allActions.length < 1 ) {
     list.innerHTML = `<li>Enter an item to get started</li>`;
     return;
-  } else if ( !items && Object.keys(allActions).length > 0 ) {
-      list.innerHTML = allActions.map((action, i) => {
-      document.documentElement.style.setProperty(`--action${i}`, action.color);
-        return `
-          <li class="action" style="color:var(--action${i}); border: 1px solid var(--action${i});">
-            <input type="checkbox" data-index=${i} id="action${i}" ${action.done ? 'checked' : ""} />
-            <label for="action${i}">${action.name}</label>
-          </li>
-        `;
-      }).join("")
-
-      return
   }
 
-  list.innerHTML = items.map((item, i) => {
-   document.documentElement.style.setProperty(`--action${i}`, item.color);
+  list.innerHTML = allActions.map( (action, i) => {
+    document.documentElement.style.setProperty(`--action${i}`, action.color);
     return `
       <li class="action" style="color:var(--action${i}); border: 1px solid var(--action${i});">
-        <input type="checkbox" data-index=${i} id="action${i}" ${item.done ? 'checked' : ""} />
-        <label for="action${i}">${item.name}</label>
+        <input type="checkbox" data-index=${i} id="action${i}" ${action.done[date] ? 'checked' : ""} />
+        <label for="action${i}">${action.name}</label>
       </li>
     `;
   }).join("")
@@ -117,11 +97,12 @@ function toggleDone(e) {
   if (!e.target.matches('input')) return; // skip this unless it's an input
   const el = e.target;
   const index = el.dataset.index;
+  const currentDate = now.format('YYYY-M-D');
 
-  dailyEntries[today][index].done = !dailyEntries[today][index].done;
-  localStorage.setItem('dailyEntries', JSON.stringify(dailyEntries));
+  allActions[index].done[currentDate] = !allActions[index].done[currentDate];
+  localStorage.setItem('allActions', JSON.stringify(allActions));
 
-  populateList(dailyEntries, actionsList, today);
+  populateActions(allActions, actionsList, currentDate);
 }
 
 
@@ -136,39 +117,4 @@ dateNavBtns.forEach(dateBtn => dateBtn.addEventListener('click', handleDateNav, 
 ));
 
 displayDate(now);
-populateActions(dailyEntries, actionsList);
-
-
-
-/*******************************
-dailyEntries = {
-  2017-09-17: {
-        name: sew,
-        done: true
-      },
-      {
-        name: play,
-        done: false
-      }
-},
-  2017-09-18: {
-        name: sew,
-        done: true
-      },
-      {
-        name: play,
-        done: false
-    }
-}
-
-allActions = [
-  {
-      name: sew,
-      color: blue
-  },
-  {
-      name: play,
-      color: pink
-  }
-]
-*/
+populateActions(allActions, actionsList);
